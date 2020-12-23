5aab1aee62e8953f36e5ede18078dae4d4bccb50
Ahmed5893 <ahmed5893hneynia@gmail.com>
2020-12-23 10:17:58 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React,{Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count:0,
+      FullName:'Ahmed',
+      bio:26,
+      job:'Developper',
+      picture:'https://fastly.syfy.com/sites/syfy/files/styles/1200x680/public/2019/04/aothero.jpg?offset-x=0&offset-y=48',
+    };
+  }
+    showProfile=()=>{
+      this.setState({show:!this.state.show});
+    }
+    
+    componentDidMount(){
+      setInterval(
+        ()=>{
+          this.setState({
+            count:this.state.count+1
+          })
+        },1000
+      )
+    }
+  render() {
+    return (
 
-export default App;
+      <div>
+        <p>
+          {this.state.count}
+        </p>
+        <button
+        type='button'
+        onClick={this.showProfile}> {this.state.show ?"hide profile":'show profile'} </button>
+        {this.state.show ?(<div className='container'>
+          <h1>{this.state.FullName}</h1>
+          <h5>{this.state.bio}</h5>
+          <h3>{this.state.job}</h3>
+          <div><img src={this.state.picture} alt='pic'/> </div>
+        </div>):(<h4>click the button to display the profile</h4>)}
+      </div>
+    );
+  }
+}
